7fc54b2fdf412c12abc1bc88e38cda9b614ceca0
nguyenvanduong20140887 <nguyenvanduong.vn.k59@gmail.com>
2019-03-13 18:15:35 +0700

    mata-bug

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,10 @@ class Product < ApplicationRecord
 
   has_many :likes
 
+  has_many :order_items
+
+  default_scope { where(active: true) }
+
   def is_liked user
     if user
       Like.find_by(user_id: user.id, product_id: id)
