7d33fdf410796b61633c50713d783359a84f8584
nguyenvanduong20140887 <nguyenvanduong.vn.k59@gmail.com>
2019-03-27 09:42:03 +0700

    fix-home-page-v1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,11 @@
 //= require popper
 //= require bootstrap-sprockets
 //= require toastr
+//= require jquery-3.2.1.min
+//= require bootstrap.min
+//= require jquery.slicknav.min
+//= require owl.carousel.min
+//= require jquery.nicescroll.min
+//= require jquery.zoom.min
+//= require jquery-ui.min
+//= require main
